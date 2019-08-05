a141f97c3987ae39024143fb684ac2b93ab7358b
hogeline <s160708@kushiro.koisen-ac.jp>
2019-08-05 17:35:27 +0900

    Update error handring

--- main.rb
+++ main.rb
@@ -1,19 +1,7 @@
 begin
-  # File.openはファイルをオープンし、Fileオブジェクトを返す
-  # 第1引数: ファイルパス
-  # 第2引数: ファイルモード (デフォルト => 'r')
-  # 第3引数: ファイルを生成する場合のパーミッション(デフォルト => 0666)
-  # 失敗した場合にErrno::EXXX例外が発生
-  #
-  # File.openにブロックを渡すと、
-  # ブロックが終了した時点でファイルを自動でクローズする
-  # フォーマットされた文字列を入れる変数
   report = ""
+  # ファイル読み込み
   File.open('report.txt') do |file|
-    # IO#each_lineは1行ずつ文字列として読み込み、それを引数にブロックを実行する
-    # 第1引数: 行の区切り文字列
-    # 第2引数: 最大の読み込みバイト数
-    # 読み込み用にオープンされていない場合にIOError
     file.each_line do |line|
       # 読み込んだ行がsectionやsubsectionだったら
       if line.match(/^[0-9]+.*$/)
@@ -24,11 +12,16 @@ begin
         report += "\n" + line
         next
       end
-      # 読み込んだlineの改行を消して先頭にスペースを入れて文字列を結合
-      report += " " + line.chomp!
+      # 文字列の最後が-なら-を消して行を結合
+      if report[-1] == "-"
+        report = report.chomp("-") + line.chomp
+        next
+      end
+      report += " " + line.chomp
     end
     puts report
   end
+  # ファイル書き込み
   File.open("format-report.txt", "w") do |f|
     f.puts(report)
   end
